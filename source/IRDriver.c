fc856ea6b7f8c08674c471035f6bc4965fcdbcab
suttonnico <suttonnico2@gmail.com>
2018-11-16 20:01:18 -0300

    Add files via upload

--- source/IRDriver.c
+++ source/IRDriver.c
@@ -25,34 +25,119 @@ uint8_t lastBits[15];
 
 // Primero busco un rising edge, significa 
 
-void IC_PISR (void){
+void IC_PISR (void)
+{
 
 	FTM_ClearInterruptFlag( FTMNumber, FTMChannel );
 
 	static uint16_t first = 1, last, result;
 	static uint8_t cont = 1;
+	static uint16_t first2 = 1, last2, result2;
 	static uint8_t firstTime = 1;
+	static uint8_t firstHBT = 1;
 
 	GPIO_SetPin(PTC, 3);
 
-	if(firstTime){
+	if(firstTime)
+	{
 		first = FTM_GetCounter( FTMNumber, FTMChannel );
+		first2 = first;
 		FTM_SetInputCaptureEdge (FTMNumber, FTMChannel,FTM_eRising);
 		firstTime = 0;
+		firstHBT = 1;
+		bits[0] = 1;
+		cont = 0;
 	}
+	else
+	{
+		last = FTM_GetCounter( FTMNumber, FTMChannel );
+		result = last - first;
+		if (result > 10500)
+		{
+			FTM_SetInputCaptureEdge (FTMNumber, FTMChannel,FTM_eFalling);
+			firstTime = 1;
 
-	last = FTM_GetCounter( FTMNumber, FTMChannel );
+			result2 = last - first2 ;
+			first2 = last;
+			cont++;
+			if ( result2 >= 2*HBT && result2 < 3*HBT )
+			{
+				if ( bits[cont-1] )
+					bits[cont]=1;
+				else
+					bits[cont]=0;
+			}
+			if ( result2 >= 3*HBT && result2 < 4*HBT )
+			{
+				if ( bits[cont-1] )
+				{
+					bits[cont]=1;
+					cont++;
+					bits[cont]=0;
+				}
+				else
+					bits[cont]=1;
+			}
+			if ( result2 >= 4*HBT )
+			{
+				bits[cont]=1;
+				cont++;
+				bits[cont]=0;
+			}
 
-	result = last - first;
-	if (result > 10000){
-		FTM_SetInputCaptureEdge (FTMNumber, FTMChannel,FTM_eFalling);
-		firstTime = 1;
+
+			if ( cont >= 14 )
+			{
+				for ( int i = 0; i < 15; i++)
+				{
+					lastBits[i] = bits[i];
+					bits[i] = 0;
+					cont = 0;
+
+				}
+				PRINTF("%d\n",getValue());
+			}
+
+		}
+		else
+		{
+			result2 = last - first2 + firstHBT * HBT;
+			firstHBT = 0;
+			first2 = last;
+			cont++;
+			if ( cont == 13){
+				__asm("NOP");
+			}
+			if ( result2 >= 2*HBT && result2 < 3*HBT )
+			{
+				if ( bits[cont-1] )
+					bits[cont]=1;
+				else
+					bits[cont]=0;
+			}
+			if ( result2 >= 3*HBT && result2 < 4*HBT )
+			{
+				if ( bits[cont-1] )
+				{
+					bits[cont]=1;
+					cont++;
+					bits[cont]=0;
+				}
+				else
+					bits[cont]=1;
+			}
+			if ( result2 >= 4*HBT )
+			{
+				bits[cont]=1;
+				cont++;
+				bits[cont]=0;
+			}
+		}
+
+
+
 	}
-
 	GPIO_ClearPin(PTC, 3);
-
-//	FTM_SetInputCaptureEdge (FTMNumber, FTMChannel,FTM_eFalling);
-					// Limpio los flags de interrupcion
 }
 
 
